0c4e16fb3756880718c831dc34bbc3642ce7b320
cron <bitreet98@gmail.com>
2021-09-20 10:43:42 +0900

    update Ping Command

--- src/main/java/com/jagrosh/jmusicbot/commands/general/PingCommand.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/general/PingCommand.java
@@ -1,2 +1,36 @@
-package com.jagrosh.jmusicbot.commands.general;public class PingCommand {
+package com.jagrosh.jmusicbot.commands.general;
+
+import java.time.temporal.ChronoUnit;
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import com.jagrosh.jdautilities.doc.standard.CommandInfo;
+import com.jagrosh.jdautilities.examples.doc.Author;
+
+/**
+ *
+ * @author John Grosh (jagrosh)
+ */
+@CommandInfo(
+        name = {"Ping", "Pong"},
+        description = "Checks the bot's latency"
+)
+@Author("John Grosh (jagrosh)")
+public class PingCommand extends Command {
+
+    public PingCommand()
+    {
+        this.name = "ping";
+        this.help = "checks the bot's latency";
+        this.guildOnly = false;
+        this.aliases = new String[]{"pong"};
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        event.reply("Ping: ...", m -> {
+            long ping = event.getMessage().getTimeCreated().until(m.getTimeCreated(), ChronoUnit.MILLIS);
+            m.editMessage("Ping: " + ping  + "ms | Websocket: " + event.getJDA().getGatewayPing() + "ms").queue();
+        });
+    }
+
 }
